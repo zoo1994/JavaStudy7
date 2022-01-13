c4c98aa84ef9037dd89984bd64925daafcd5ba3d
zoo1994 <composure1994@gmail.com>
2022-01-13 16:35:01 +0900

    1

--- src/com/sj/s1/phonebook/PhonebookDTO.java
+++ src/com/sj/s1/phonebook/PhonebookDTO.java
@@ -1,5 +1,42 @@
 package com.sj.s1.phonebook;
 
 public class PhonebookDTO {
-
+//	이름, 별명, 주소, 전화번호, 생일
+	private String name;
+	private String nickname;
+	private String email;
+	private String phonenumber;
+	private String birthday;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
 }
---
+++ src/com/sj/s1/phonebook/PhonebookMain.java
@@ -0,0 +1,10 @@
+package com.sj.s1.phonebook;
+
+public class PhonebookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
